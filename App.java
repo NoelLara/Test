ba683059fb255b40cb9f4b3134c65c2249981766
NoelLara <larafalfannoel@gmail.com>
2023-04-06 18:40:35 -0600

    add greeting func

--- App.java
+++ App.java
@@ -1,6 +1,10 @@
 public class App{
+    public static void greeting(){
+        System.out.println("Hola bb");
+    }
     public static void main(String[] args){
-        System.out.println("Hello, wordl");
+        System.out.println("Hello, wordl :D");
         System.out.println("Hi, Brayan");
+        greeting();
     }
 }
